5287b35dca1a16d11687de4f26aa8d6d88c8b343
Johnny <johnny.griff04@gmail.com>
2026-07-02 17:19:14 +0200

    comments + rename

--- FilteringCASCIOutput.py
+++ FilteringCASCIOutput.py
@@ -8,7 +8,7 @@ pd.set_option('display.max_rows', None)
 # RegEx, creates markers/patterns to get only specific data frfom the files 
 import re
 
-def ReadDesiredEnergiesFunc(FilePath):
+def ReadDesiredEnergiesFunc(FilePath: str) -> int:
     
     # The line starts after 'CASCI converged'
     pattern = r'CASCI converged'
@@ -21,7 +21,7 @@ def ReadDesiredEnergiesFunc(FilePath):
                 # Return index of that line
                 return linecounter
             
-def ReadSimEnergiesFunc(FilePath):
+def ReadSimEnergiesFunc(FilePath: str) -> int:
     
     # The line starts with State'
     pattern = r'^State'
@@ -34,7 +34,7 @@ def ReadSimEnergiesFunc(FilePath):
                 # Return index of that line
                 return linecounter
             
-def CASCITheoryData(FilePath, SkipRows):
+def CASCITheoryData(FilePath: str, SkipRows: int) -> list[dict]:
     """
     Full CI data from output file
     """
@@ -47,10 +47,12 @@ def CASCITheoryData(FilePath, SkipRows):
     TableLines = TableLines[SkipRows+1:]
     # Filter only desired spin state
     TableLines = [Line for Line in TableLines if "S^2 = 0.0000000" in Line]
+    # Obtain from each line only the state number and energy (Eh)
     sepPattern = re.compile(
         r"(?P<State>\d{1,3}\s*)"
         r"E\s*=\s+(?P<E>[-]\d[.]\d{14})"
     )
+    # Store these in new list[dict]
     DesiredData = []
     for Line in TableLines:
         match = re.search(sepPattern,Line)
@@ -60,22 +62,23 @@ def CASCITheoryData(FilePath, SkipRows):
                 "E":float(match.group('E'))})
     return DesiredData
     
-def CASCISimData(FilePath,SkipRows):
+def CASCISimData(FilePath: str,SkipRows: int) -> list[dict]:
     """
     Simulation data
     """
-    from io import StringIO
+    
     #Stores each line in output file
     with open(FilePath,"r") as f:
         TableLines = f.readlines()
     #Stores the lines after the amount specified by skiprows
     TableLines = TableLines[SkipRows+1:]
-    # Filter only desired spin state
+    # Obtain from each line the state number, the energy (Eh) and the excitation energy (eV)
     sepPattern = re.compile(
         r"\s*(?P<State>\d{1,3}\s*)"
         r"\s+(?P<E>[-]\d[.]\d{8})"
         r"\s+(?P<Exc>\d{1,3}[.]\d{6})"
     )
+    # Store these in new list[dict]
     SimData = []
     for Line in TableLines:
         match = re.search(sepPattern,Line)
@@ -86,14 +89,18 @@ def CASCISimData(FilePath,SkipRows):
                 "Excitation energy (eV)":float(match.group('Exc'))})
     return SimData
 
-def CleanData(DesiredValues: list[dict], SimValues: list[dict]) -> list[dict]:
+def ComparisonData(DesiredValues: list[dict], SimValues: list[dict]) -> list[dict]:
+    """
+    Compares the values produced by simulation to CI calculation
+    Removes all that are not the same (error from pySCF not maintaining desired spin symmetry)
+    """
     desiredEnergy = {"%.8f"%(d['E']) for d in DesiredValues}
     desiredEnergy = {float(i) for i in desiredEnergy}
-    cleaned = []
+    FilteredData = []
     for energy in SimValues:
         if energy['Energy (Eh)'] in desiredEnergy:
-            cleaned.append(energy)
-    return cleaned
+            FilteredData.append(energy)
+    return FilteredData
 
 FilePath = "./test200root.out"
 SkipRows = ReadDesiredEnergiesFunc(FilePath)
@@ -102,6 +109,6 @@ CASCIDesiredEnergies = CASCITheoryData(FilePath, SkipRows)
 SkipRows = ReadSimEnergiesFunc(FilePath)
 CASCISimEnergies = CASCISimData(FilePath, SkipRows)
 
-result = CleanData(CASCIDesiredEnergies, CASCISimEnergies)
-df = pd.DataFrame(result)
+FilteredData = ComparisonData(CASCIDesiredEnergies, CASCISimEnergies)
+df = pd.DataFrame(FilteredData)
 print(df)
