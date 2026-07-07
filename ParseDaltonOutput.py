fe670360f37f649fb7c2f8bcc52c5f305d4644eb
Johnny <johnny.griff04@gmail.com>
2026-07-07 12:51:23 +0200

    adjusted regex

--- ParseDaltonOutput.py
+++ ParseDaltonOutput.py
@@ -10,50 +10,51 @@ import re
                      
 def CASCISimData(FilePath: str) -> pd.DataFrame:
     """
-    Parse values for excitatoin energies and total oscillator strength from each excited state 
+    Parse values for excitation energies and total oscillator strength from each excited state 
     generated in output file by Dalton
     """
     #Stores each line in output file
     with open(FilePath,"r") as f:
         TableLines = f.readlines()
 
-
     # Obtain from each line the state number, the energy (Eh) and the excitation energy (eV)
     ExcELines = [Line for Line in TableLines if "@ Excitation energy :" in Line]
     OscStrLines =  [Line for Line in TableLines if "@ Total oscillator strength " in Line ]
     ExcELines = [elem.replace("@", "") for elem in ExcELines]
     OscStrLines = [elem.replace("@", "") for elem in OscStrLines]
-    
+
     #Get only the actual numbers
     EnergPattern = re.compile(
-        r"\s+(?P<E>\d[.]\d{7,8})"
+        r"\s+(?P<E>\d+\.\d+)"
         )
     OscStrPattern = re.compile(
         r"\s+(?P<OscStr>\d+\.\d+[eE]?[-+]?\d+)"
     )
     # Store these in new list[dict]
     EnergData = []
-    for Line in ExcELines:
-        match = re.search(EnergPattern,Line)
+    for EnergyLine in ExcELines:
+        match = re.search(EnergPattern, EnergyLine)
         if match:
-            EnergData.append(float(match.group('E'))
+            EnergData.append(float(match.group('E'))*27.2114
             )
     OscStrData = []
-    for line in OscStrLines:
-        match = re.search(OscStrPattern, line)
+    for OscStrLine in OscStrLines:
+        match = re.search(OscStrPattern, OscStrLine)
         if match:
             OscStrData.append(float(match.group('OscStr'))
             )
-    
+    print(len(EnergData))
+    print(len(OscStrData))
     #Store in dataframe
-    SimDataDF = pd.DataFrame({"Excitation energy (a.u)": EnergData,"Total oscillator strength": OscStrData})
+    SimDataDF = pd.DataFrame({"Excitation energy (eV)": EnergData,"Total oscillator strength": OscStrData})
 
     return SimDataDF
 
 
-FilePath = "./CAS46_lih.out"
+FilePath = "./DaltonOutputs/CAS44_beh2_pvdz.out"
 
-CASCISimEnergiesDF = CASCISimData(FilePath)#, SkipRows)
+CASCISimEnergiesDF = CASCISimData(FilePath)
 # The index count is the same as the root set in the Dalton file, i.e the number of excited states
 CASCISimEnergiesDF.index +=1
+print("Taken from:", FilePath)
 print(CASCISimEnergiesDF)
